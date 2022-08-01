3cfb769de02975782ecd7c30d30dfbce1ec3c07f
ivan <ivan.proskuryakov.2001@yandex.ru>
2022-08-01 20:36:08 +0300

    Добавлен обработчик для команды /keys

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app
+from flask import request
+import json
 
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/keys', methods=['GET'])
+def keys_parser():
+    j_data = request.get_json()
+    return json.dumps(list(j_data.keys()))
